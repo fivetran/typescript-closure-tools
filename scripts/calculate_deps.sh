391630ea60667eda47d934461dbec42db9bbc8dc
Georgie <george@fivetran.com>
2014-04-19 20:22:57 -0700

    Use closurebuilder.py to insert reference tags

--- scripts/calculate_deps.sh
+++ scripts/calculate_deps.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 python lib/closure-library/closure/bin/build/closurebuilder.py \
   --root=lib/closure-library/ \
-  --root=src/scripts/model/ \
-  --root=src/scripts/view/ \
-  --root=src/scripts/directives/ \
-  --root=src/scripts/controllers/ \
-  --root=src/scripts/filters/ \
-  --namespace "$1"
+  --input "$1"
