699eb7e792783525500a50ef7313e2cfdc4c94f6
Georgie <george@fivetran.com>
2014-05-05 11:57:44 -0700

    CLOSURE: work around goog.string being illegal module name

--- index/closure/goog/string/stringbuffer.d.ts
+++ index/closure/goog/string/stringbuffer.d.ts
@@ -2,17 +2,10 @@
 
 /// <reference path="../../../closure/goog/base.d.ts" />
 
-declare module goog.string {
+declare module goog {
 
-    /**
-     * Utility class to facilitate string concatenation.
-     *
-     * @param {*=} opt_a1 Optional first initial item to append.
-     * @param {...*} var_args Other initial items to
-     *     append, e.g., new goog.string.StringBuffer('foo', 'bar').
-     * @constructor
-     */
-    class StringBuffer {
+    module _string {
+
         /**
          * Utility class to facilitate string concatenation.
          *
@@ -21,39 +14,49 @@ declare module goog.string {
          *     append, e.g., new goog.string.StringBuffer('foo', 'bar').
          * @constructor
          */
-        constructor(opt_a1?: any, ...var_args: any[]);
-    
-        /**
-         * Sets the contents of the string buffer object, replacing what's currently
-         * there.
-         *
-         * @param {*} s String to set.
-         */
-        set(s: any): void;
-    
-        /**
-         * Appends one or more items to the buffer.
-         *
-         * Calling this with null, undefined, or empty arguments is an error.
-         *
-         * @param {*} a1 Required first string.
-         * @param {*=} opt_a2 Optional second string.
-         * @param {...*} var_args Other items to append,
-         *     e.g., sb.append('foo', 'bar', 'baz').
-         * @return {goog.string.StringBuffer} This same StringBuffer object.
-         * @suppress {duplicate}
-         */
-        append(a1: any, opt_a2?: any, ...var_args: any[]): goog.string.StringBuffer;
-    
-        /**
-         * Clears the internal buffer.
-         */
-        clear(): void;
-    
-        /**
-         * @return {number} the length of the current contents of the buffer.
-         */
-        getLength(): number;
+        class StringBuffer {
+            /**
+             * Utility class to facilitate string concatenation.
+             *
+             * @param {*=} opt_a1 Optional first initial item to append.
+             * @param {...*} var_args Other initial items to
+             *     append, e.g., new goog.string.StringBuffer('foo', 'bar').
+             * @constructor
+             */
+            constructor(opt_a1?: any, ...var_args: any[]);
+
+            /**
+             * Sets the contents of the string buffer object, replacing what's currently
+             * there.
+             *
+             * @param {*} s String to set.
+             */
+            set(s: any): void;
+
+            /**
+             * Appends one or more items to the buffer.
+             *
+             * Calling this with null, undefined, or empty arguments is an error.
+             *
+             * @param {*} a1 Required first string.
+             * @param {*=} opt_a2 Optional second string.
+             * @param {...*} var_args Other items to append,
+             *     e.g., sb.append('foo', 'bar', 'baz').
+             * @return {goog.string.StringBuffer} This same StringBuffer object.
+             * @suppress {duplicate}
+             */
+            append(a1: any, opt_a2?: any, ...var_args: any[]): goog.string.StringBuffer;
+
+            /**
+             * Clears the internal buffer.
+             */
+            clear(): void;
+
+            /**
+             * @return {number} the length of the current contents of the buffer.
+             */
+            getLength(): number;
+        }
     }
 }
 
