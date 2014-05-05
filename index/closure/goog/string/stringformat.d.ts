960d7567525f68f8100c248dc418ae35ae037882
Georgie <george@fivetran.com>
2014-05-05 11:57:44 -0700

    CLOSURE: work around goog.string being illegal module name

--- index/closure/goog/string/stringformat.d.ts
+++ index/closure/goog/string/stringformat.d.ts
@@ -3,17 +3,20 @@
 /// <reference path="../../../closure/goog/base.d.ts" />
 /// <reference path="../../../closure/goog/string/string.d.ts" />
 
-declare module goog.string {
+declare module goog {
 
-    /**
-     * Performs sprintf-like conversion, ie. puts the values in a template.
-     * DO NOT use it instead of built-in conversions in simple cases such as
-     * 'Cost: %.2f' as it would introduce unneccessary latency oposed to
-     * 'Cost: ' + cost.toFixed(2).
-     * @param {string} formatString Template string containing % specifiers.
-     * @param {...string|number} var_args Values formatString is to be filled with.
-     * @return {string} Formatted string.
-     */
-    function format(formatString: string, ...var_args: any /*string|number*/[]): string;
+    module _string {
+
+        /**
+         * Performs sprintf-like conversion, ie. puts the values in a template.
+         * DO NOT use it instead of built-in conversions in simple cases such as
+         * 'Cost: %.2f' as it would introduce unneccessary latency oposed to
+         * 'Cost: ' + cost.toFixed(2).
+         * @param {string} formatString Template string containing % specifiers.
+         * @param {...string|number} var_args Values formatString is to be filled with.
+         * @return {string} Formatted string.
+         */
+        function format(formatString: string, ...var_args: any /*string|number*/[]): string;
+    }
 }
 
