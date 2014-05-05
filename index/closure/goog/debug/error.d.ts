792cadd5b18409a4143e5184c2c3270982ff6d0d
Georgie <george@fivetran.com>
2014-05-05 16:01:48 -0700

    CLOSURE: work around Error namespace shadow

--- index/closure/goog/debug/error.d.ts
+++ index/closure/goog/debug/error.d.ts
@@ -1,5 +1,9 @@
 /// <reference path="../../../closure/goog/base.d.ts" />
 
+declare module goog._debug {
+    interface _Error extends Error { }
+}
+
 declare module goog.debug {
 
     /**
@@ -8,7 +12,7 @@ declare module goog.debug {
      * @constructor
      * @extends {Error}
      */
-    class Error extends Error {
+    class Error implements goog._debug._Error {
         /**
          * Base class for custom error objects.
          * @param {*=} opt_msg The message associated with the error.
@@ -16,6 +20,10 @@ declare module goog.debug {
          * @extends {Error}
          */
         constructor(opt_msg?: any);
+
+        name: string;
+
+        message: string;
     }
 }
 
