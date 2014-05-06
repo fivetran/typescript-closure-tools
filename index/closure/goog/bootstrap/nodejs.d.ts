dbc46f7c6156ba7708023259fb65c936aea175c8
Georgie <george@fivetran.com>
2014-05-06 15:11:06 -0700

    CLOSURE: conflicts with node definition

--- index/closure/goog/bootstrap/nodejs.d.ts
+++ index/closure/goog/bootstrap/nodejs.d.ts
@@ -1,20 +1,4 @@
 
-declare module global {
-
-    /**
-     * The goog namespace in the global scope.
-     */
-    var goog: any /*missing*/;
-
-    /**
-     * Imports a script using Node's require() API.
-     *
-     * @param {string} src The script source.
-     * @return {boolean} True if the script was imported, false otherwise.
-     */
-    function CLOSURE_IMPORT_SCRIPT(src: string): boolean;
-}
-
 declare module goog {
 
     /**
