34be170e4b39f32a8b2a6190ab046339367b32ee
Georgie <george@fivetran.com>
2014-05-07 11:56:16 -0700

    Cache parsed files

---
+++ scripts/finder.ts
@@ -0,0 +1,41 @@
+/// <reference path="../index/node.d.ts"/>
+
+import fs = require('fs');
+import parser = require('./parser');
+import combine = require('./combine');
+
+var fileByProvide: { [key: string]: string } = {};
+
+fs.readFileSync('symbols.tsv', 'utf8').split('\n').forEach(line => {
+    var columns = line.split('\t');
+    var file = columns[0];
+    var symbol = columns[1];
+
+    fileByProvide[symbol] = file;
+});
+
+interface FileCache {
+    [fileName: string]: combine.Symbols;
+}
+
+var providesByFile: FileCache = {};
+
+/**
+ * @param symbolName
+ * @returns file that goog.provide's name
+ */
+export function file(symbolName: string): string {
+    return fileByProvide[symbolName];
+}
+
+/**
+ * @param fileName
+ * @returns value of name
+ */
+export function symbols(fileName: string): combine.Symbols {
+    if (!providesByFile[fileName])
+        providesByFile[fileName] = combine.members(parser.jsdoc(fileName));
+
+    if (providesByFile[fileName])
+        return providesByFile[fileName];
+}
