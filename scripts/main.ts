e6614a6de5553c9d010eaf96139ce1077f518c72
Georgie <george@fivetran.com>
2014-05-07 16:45:54 -0700

    Take input output as arguments

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,11 +1,15 @@
 /// <reference path="../index/node.d.ts"/>
 
+import fs = require('fs');
 import finder = require('./finder');
 import generate = require('./generate');
 import pretty_print = require('./pretty_print');
+import options = require('./options');
 
-var symbols = finder.symbols('lib/closure-library/closure/goog/promise/promise.js');
-var defs = generate.defs(symbols);
-var pretty = pretty_print.pretty(defs);
+options.todo.forEach(todo => {
+    var symbols = finder.symbols(todo.input);
+    var defs = generate.defs(symbols);
+    var pretty = pretty_print.pretty(defs);
 
-console.log(pretty);
+    fs.writeFile(todo.output, pretty);
+});
