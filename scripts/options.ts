64c2a4a55b74edd10fb265a4a677b375af29cb41
Georgie <george@fivetran.com>
2014-05-07 16:42:35 -0700

    Arguments should be input / output pairs

--- scripts/options.ts
+++ scripts/options.ts
@@ -1,19 +1,23 @@
 /// <reference path="../index/node.d.ts"/>
 
+export interface InputOutput {
+    input: string;
+    output: string;
+}
+
 export var provides: string;
-export var js: string[] = [];
+export var todo: InputOutput[] = [];
 
-var currentArgument;
+var options = process.argv.slice(2);
 
-process.argv.slice(2).forEach(name => {
-    if (name === '--provides')
-        currentArgument = 'provides';
-    else if (name === '--js')
-        currentArgument = 'js';
-    else if (name.charAt(0) === '-')
-        console.error('Usage: jsdoc2ts [--provides provides_file.tsv] --js source_file.js [more_source_files.js...]');
-    else if (currentArgument === 'provides')
-        provides = name;
-    else if (currentArgument === 'js')
-        js.push(name);
-});
+if (options[0] === '--provides') {
+    provides = options[1];
+    options = options.slice(2);
+}
+
+for (var i = 0; i < options.length; i += 2) {
+    todo.push({
+        input: options[i],
+        output: options[i + 1]
+    });
+}
