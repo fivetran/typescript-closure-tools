a9787ef82869d5295c81d28a1780621571190336
Georgie <george@fivetran.com>
2014-05-08 18:33:50 -0700

    Delete old generated files

--- index/closure/_window.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Alisases for symbols that get shadowed by goog symbols
- */
-declare module _window {
-    interface _File extends File { }
-}
--- index/closure/goog/editor/defines.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="../../../closure/goog/base.d.ts" />
-
