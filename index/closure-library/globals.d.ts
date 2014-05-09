ce77e61bda6137b75c8b21ec11e87a9fa8f398e5
Georgie <george@fivetran.com>
2014-05-08 18:42:53 -0700

    CLOSURE: Missing symbols

--- index/closure-library/globals.d.ts
+++ index/closure-library/globals.d.ts
@@ -5,3 +5,21 @@ declare module goog {
 
     export import string = _string;
 }
+
+declare class _Error implements Error {
+    name: string;
+    message: string;
+}
+
+interface Image extends HTMLImageElement { }
+
+interface IThenable<R> {
+    then<U>(onFulfilled: (value: R) => IThenable<U>,  onRejected: (error: any) => IThenable<U>): IThenable<U>;
+    then<U>(onFulfilled: (value: R) => IThenable<U>, onRejected?: (error: any) => U): IThenable<U>;
+    then<U>(onFulfilled: (value: R) => U, onRejected: (error: any) => IThenable<U>): IThenable<U>;
+    then<U>(onFulfilled?: (value: R) => U, onRejected?: (error: any) => U): IThenable<U>;
+}
+
+interface IDBKeyType { }
+
+interface IDBDatabaseException { }
