4cac842d3b67bb9132cb566ff86a47df94cac14d
Georgie <george@fivetran.com>
2015-07-10 12:55:14 -0700

    Don't bother with errors.txt

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Run tsc on each declaration, redirecting errors to errors.txt
-rm -f errors.txt
-touch errors.txt
+# Run tsc on each declaration
 
 FILES=$(find index/closure-library -name '*.d.ts')
 
